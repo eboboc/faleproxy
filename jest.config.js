3eeb38f870b5ec7165aee750409637314a37883f
Eca Boboc <eboboc@college.harvard.edu>
2025-04-07 15:09:22 -0400

    Improve Jest configuration to collect coverage for application code

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  // Specify which files to collect coverage from
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/tests/**',
+    '!jest.config.js'
+  ],
   coverageThreshold: {
     global: {
       branches: 0,
@@ -19,5 +27,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
   // Exclude node_modules from coverage
-  coveragePathIgnorePatterns: ['/node_modules/']
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/tests/']
 };
